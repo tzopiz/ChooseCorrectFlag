ce02d46772972b03645632aa7d3753655c51190a
Dmitry <dmitry.korchagin.s@gmail.com>
2023-04-04 00:12:23 +0300

    Initial Commit

---
+++ ChooseRightFlag/ChooseRightFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseRightFlagApp.swift
+//  ChooseRightFlag
+//
+//  Created by Дмитрий Корчагин on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChooseRightFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
